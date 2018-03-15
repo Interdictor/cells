c2291b6a71a08da4b7191f6edfd78e7d4fa5369a
interdictor <interdictor.dsg@gmail.com>
2018-03-15 17:26:31 +0100

    Implement styles

--- public/javascript/components/cell_map.js
+++ public/javascript/components/cell_map.js
@@ -6,5 +6,17 @@ Vue.component('cell-map', {
       </li>
     </ul>
   `,
-  props: ['cells']
+  props: ['row_size', 'column_size'],
+  computed: {
+    cells: function() {
+      const result = []
+      for (row_index = 0; row_index < this.row_size; row_index++) {
+        for (column_index = 0; column_index < this.column_size; column_index++) {
+          result.push([row_index, column_index])
+        }
+      }
+      console.log(result)
+      return result
+    }
+  }
 })
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,8 @@
 new Vue({
   el: '#main',
   data: {
-    cell_descriptors: [...Array(4094).keys()]
+    // cell_descriptors: [...Array(4094).keys()],
+    row_size: 100,
+    column_size: 60
   }
 })
