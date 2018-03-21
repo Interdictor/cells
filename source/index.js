87030f530d2a83830e7ed40e9d749afc570e52f9
interdictor <interdictor.dsg@gmail.com>
2018-03-21 02:39:34 +0100

    Bundle domain objects

---
+++ source/index.js
@@ -0,0 +1,4 @@
+global.Coordinate = require('./domain/coordinate.js')
+global.Cell = require('./domain/cell.js')
+global.Rules = require('./domain/rules.js')
+global.Tissue = require('./domain/tissue.js')
